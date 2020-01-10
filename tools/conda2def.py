36a0e4b11a3ddd7809e48fba58b628c6354b6361
Andrea Telatin <andrea@telatin.com>
2020-01-10 15:09:57 +0000

    Draft

--- tools/conda2def.py
+++ tools/conda2def.py
@@ -3,6 +3,8 @@
 import sys
 import argparse
 import pprint
+import yaml
+
 def eprint(*args, **kwargs):
     """print to STDERR"""
     print(*args, file=sys.stderr, **kwargs)
@@ -12,6 +14,34 @@ def vprint(*args, **kwargs):
         return 0 
     eprint(*args, **kwargs)
 
+def makeDefFromYaml():
+    print("""
+Bootstrap: docker
+
+From: continuumio/miniconda3
+
+%files
+ {0}
+
+%environment
+ PATH=/opt/conda/envs/{1}/bin:$PATH
+
+%post
+ echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
+ echo "source activate {1}" > ~/.bashrc
+ /opt/conda/bin/conda env create -f env.yaml
+
+%runscript
+ exec "$@"
+""".format(
+    opt.env_file,
+    env_name
+))
+    
+
+def makeDefFromList():
+    pass
+
 opt_parser = argparse.ArgumentParser(description='Create a Singularity definition file from a list of Conda packages')
 
 opt_parser.add_argument('-e', '--env-file',
@@ -19,6 +49,10 @@ opt_parser.add_argument('-e', '--env-file',
 
 opt_parser.add_argument('-c', '--channel',
                         help='Conda channel (when not using --env-file)',
+                        action='append')
+
+opt_parser.add_argument('-p', '--package',
+                        help='Conda package(s) (when not using --env-file)',
                         action='append')
 
 
@@ -30,5 +64,23 @@ opt_parser.add_argument('-v', '--verbose',
 opt = opt_parser.parse_args()
 
 if __name__ == '__main__':
-    for c in opt.channel:
-        vprint(' - Adding channel {}'.format(c))
+    
+    if opt.env_file != None:
+        vprint('loading: {}'.format(opt.env_file))
+        try:
+            with open(opt.env_file, 'r') as stream:
+                try:
+                    data = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    eprint('\nFATAL YAML ERROR:\ntrying to parse <{}>:\n{}'.format(opt.env_file,exc))
+        except Exception as exc:
+                    eprint('\nFATAL ERROR:\nTrying to read {}:\n{}'.format(opt.env_file,exc))
+
+        env_name = data['name']
+        makeDefFromYaml()
+    else:
+        if opt.package == None:
+            eprint("FATAL ERROR:\nSpecify at least a package with -p PACKAGE (or env file)\nTry --help")
+            exit()
+        else:
+            pass
