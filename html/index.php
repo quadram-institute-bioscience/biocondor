18a430aee4995fc1850c7bc808fe16c6a7b656a4
Andrea Telatin <andrea@telatin.com>
2020-01-13 07:17:16 -0800

    Added library

--- html/index.php
+++ html/index.php
@@ -101,11 +101,11 @@ include('inc/nav.php');
 
 </div>
 
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/esm/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </body>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </html>
 
 
